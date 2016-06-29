f9bfbb7134fe5cceb8ca6a234ed51071f9940d7a
Mateusz <bryzikm@gmail.com>
2016-06-30 00:11:37 +0200

    Adding new Gaussian to Rules

--- src/com/fuzzy/FuzzyEngine.java
+++ src/com/fuzzy/FuzzyEngine.java
@@ -3,6 +3,7 @@ package com.fuzzy;
 
 import com.fuzzy.rule.RuleGenerator;
 import com.fuzzylite.Engine;
+import com.fuzzylite.term.Gaussian;
 import com.fuzzylite.term.Triangle;
 import com.fuzzylite.variable.InputVariable;
 import com.fuzzylite.variable.OutputVariable;
@@ -27,39 +28,39 @@ public class FuzzyEngine {
         this.income = new InputVariable();
         income.setName("income");
         income.setRange(20542.3650727607, 68427.163111046);
-        income.addTerm(new Triangle("low", 20542.3650727607, 28504.58, 36504.));
-        income.addTerm(new Triangle("medium", 36504., 42465.6, 52465.6));
-        income.addTerm(new Triangle("high", 52465.6, 60562.587, 68427.163111046));
+        income.addTerm(new Gaussian("low", 20542.3650727607, 11971.199509571325, 1.0));
+        income.addTerm(new Gaussian("medium", 44484.76409190335, 11971.199509571325, 1.0));
+        income.addTerm(new Gaussian("high", 68427.163111046, 11971.199509571325, 1.0));
         engine.addInputVariable(income);
 
         this.age = new InputVariable();
         age.setName("age");
-        age.setRange(18., 63.);
-        age.addTerm(new Triangle("low", 18., 24., 32.));
-        age.addTerm(new Triangle("medium", 32., 39., 47.));
-        age.addTerm(new Triangle("high", 47., 57., 63.));
+        age.setRange(18.1300383633934, 63.108049491886);
+        age.addTerm(new Gaussian("low", 18.1300383633934, 11.24450278212315, 1.0));
+        age.addTerm(new Gaussian("medium", 40.6190439276397, 11.24450278212315, 1.0));
+        age.addTerm(new Gaussian("high", 63.108049491886, 11.24450278212315, 1.0));
         engine.addInputVariable(age);
 
         this.loan = new InputVariable();
         loan.setName("loan");
-        loan.setRange(15.5, 10871.2);
-        loan.addTerm(new Triangle("low", 15.5, 24., 3633.56));
-        loan.addTerm(new Triangle("medium", 3633.56, 3642.6, 7252.65));
-        loan.addTerm(new Triangle("high", 7252.6, 9487.57, 10871.6));
+        loan.setRange(15.4985984378272, 10871.1867897838);
+        loan.addTerm(new Gaussian("low", 15.4985984378272, 2713.9220478364932, 1.0));
+        loan.addTerm(new Gaussian("medium", 5443.3426941108136, 2713.9220478364932, 1.0));
+        loan.addTerm(new Gaussian("high", 10871.1867897838, 2713.9220478364932, 1.0));
         engine.addInputVariable(loan);
 
         this.lti = new InputVariable();
         lti.setName("lti");
         lti.setRange(6.22332096099854E-4, 0.190751580661163);
-        lti.addTerm(new Triangle("low", 6.22332096099854E-4, 0.0156589879, 0.06399868));
-        lti.addTerm(new Triangle("medium",  0.06399868, 0.10024578, 0.12737509));
-        lti.addTerm(new Triangle("high", 0.12737509, 0.1576895, 0.190751580661163));
+        lti.addTerm(new Gaussian("low", 6.22332096099854E-4, 0.0475323121412657865, 1.0));
+        lti.addTerm(new Gaussian("medium",  0.095686956378631427, 0.0475323121412657865, 1.0));
+        lti.addTerm(new Gaussian("high", 0.190751580661163, 0.0475323121412657865, 1.0));
         engine.addInputVariable(lti);
 
         result = new OutputVariable();
         result.setName("result");
         result.setRange(0., 1.0);
-        result.addTerm(new Triangle("rejected", 0, 0.25, 0.5));
+        result.addTerm(new Triangle("rejected", 0, 0.25, 0.499999999));
         result.addTerm(new Triangle("approve", 0.5, 0.75, 1.0));
         engine.addOutputVariable(result);
 
